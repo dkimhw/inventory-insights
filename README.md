4d56f253548690d70914deab01d64a6fe61f150b
David Kim <dkhw90@gmail.com>
2022-08-28 03:52:14 +0200

    Refactor code to include components and allow for additional dashboard pages

---
+++ README.md
@@ -0,0 +1,20 @@
+
+## How to Run
+
+1. Clone the repository
+
+```
+git clone https://github.com/dkimhw/dealership_competitor_dash.git
+```
+
+2. Install dependencies
+
+```
+pip install -r requirements.txt
+```
+
+3. Run the application
+
+```
+python index.py
+```
---
+++ components/navbar.py
@@ -0,0 +1,22 @@
+# Import necessary libraries
+import dash
+import dash_bootstrap_components as dbc
+
+
+# Define the navbar structure
+def Navbar():
+    layout = dash.html.Div([
+        dbc.NavbarSimple(
+            children=[
+                dbc.NavItem(dbc.NavLink("Summary", href="/"), className="navbar-links-styles"),
+                dbc.NavItem(dbc.NavLink("Make Detail", href="/make-detail"), className="navbar-links-styles"),
+            ] ,
+            brand="Inventory Insights",
+            brand_href="/",
+            color="dark",
+            dark=True,
+            className="navbar-brand-styles"
+        ),
+    ])
+
+    return layout
