8b569718981a486c82e3ad1d4b35b97025ba76db
David Kim <dkhw90@gmail.com>
2022-09-30 16:54:49 +0200

    Add a new data table to make details page

--- services/make_detail_data.py
+++ services/make_detail_data.py
@@ -1,3 +1,4 @@
+from re import S
 from services.query_util import query
 
 def get_vehicle_makes():
@@ -110,33 +111,58 @@ def get_avg_vehicle_year_by_month_and_make(start_date, end_date, makes):
   makes_str = f"and make in ('{makes_str}')" if makes_str != '' else ''
 
   sql_query = f"""
-    with inventory_data as (
-      SELECT
-        vin
-        , make
-        , DATE_TRUNC('month', scraped_date) as inventory_month
-        , year
-        , row_number() OVER (PARTITION BY vin, DATE_TRUNC('month', scraped_date) ORDER BY scraped_date ASC) AS filter_row
-      FROM
-        scraped_inventory_data.inventories
-      WHERE
-        scraped_date >= '{start_date}'
-        and scraped_date <= '{end_date}'
-        {makes_str}
-    )
     select
-      inventory_month,
-      make,
+      scraped_month as inventory_month,
+      INITCAP(make) as make,
       round(avg(year), 0) as vehicle_year
     from
-      inventory_data
+      scraped_inventory_data.inventories
     where
-      filter_row = 1
+      scraped_date >= '{start_date}'
+      and scraped_date <= '{end_date}'
+      {makes_str}
     group by
-      inventory_month
-      , make
+      1, 2
     order by
-      inventory_month DESC, make ASC;
+      1 DESC, 2 ASC;
+  """
+  result = query(sql_query)
+  return result
+
+def get_make_model_trim_data(start_date, end_date, makes):
+  '''
+  Returns a dataframe containing aggregated metrics for each vehicle make model trim
+
+  :params start_date: specify the start time period for filtering out the inventory data for calculating the avg inventory year
+  :type start_date: str
+  :params end_date: specify the end time period for filtering out the inventory data for calculating the avg inventory year
+  :type end_date: str
+  :params make: list of vehicle makes to filter out the inventory data for calculating the avg inventory year
+  :type make: list
+
+  :returns: a dataframe containing aggregated metrics for each vehicle make model trim
+  :rtype: DataFrame
+  '''
+  makes_str = "','".join(makes) if makes else ''
+  makes_str = f"and make in ('{makes_str}')" if makes_str != '' else ''
+
+  sql_query = f"""
+    select
+      INITCAP(make) || ' ' || model_trim as vehicle_model_title,
+      count(distinct vin) as total_inventory,
+      round(avg(year), 0) as avg_vehicle_year,
+      round(avg(price::numeric), 0) as avg_price,
+      round(avg(mileage), 0) as avg_mileage
+    from
+      scraped_inventory_data.inventories
+    where
+      scraped_date >= '{start_date}'
+      and scraped_date <= '{end_date}'
+      {makes_str}
+    group by
+      1
+    order by
+      2 DESC;
   """
   result = query(sql_query)
   return result
